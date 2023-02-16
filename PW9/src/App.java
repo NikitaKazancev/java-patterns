7b49c2661c42bd1bdd7aff9b60cec884b9e7d6f0
Nike <nkaz2003@gmail.com>
2023-02-16 18:49:06 +0300

    pw9

---
+++ PW9/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("\n\n\n");
+
+		for (int i = 0; i < 10; i++) {
+			System.out.println("COOL! ");
+		}
+
+		System.out.println("\n\n\n");
+	}
+}
