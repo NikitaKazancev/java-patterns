b73f85370daf0a7982800e48fde13b58f41ab893
Nike <nkaz2003@gmail.com>
2023-02-16 18:49:27 +0300

    pw11

---
+++ pw11/src/main/java/com/example/pw11/users/UserRepo.java
@@ -0,0 +1,8 @@
+package com.example.pw11.users;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepo
+	extends CrudRepository<User, Long> {
+	User findByName(String name);
+}
