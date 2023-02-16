d3b82bd4facc9e1de0da9284dd9d980763c854dd
Nike <nkaz2003@gmail.com>
2023-02-16 18:49:20 +0300

    pw10

---
+++ PW10/src/main/java/com/example/pw10/MessageService.java
@@ -0,0 +1,11 @@
+package com.example.pw10;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageService {
+
+	public String getMessage() {
+		return "Hello";
+	}
+}
